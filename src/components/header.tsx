71cb58f43858d8696ae8ee95f3d2d129bb3d60b5
ejjang <cuttlefish77777@gmail.com>
2022-07-20 20:49:32 +0900

    youtube data api fetch(403error로 오늘은 종료�)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import Logo from 'assets/images/logo.png';
 import SearchBar from './searchBar';
--- src/components/itemCard.tsx
+++ src/components/itemCard.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
+interface reqProps {
+  method: string;
+  redirect: RequestRedirect;
+}
 const ItemCard = () => {
+  const [videoItem, setVideoItem] = useState([]);
+  const requestOptions: reqProps = {
+    method: 'GET',
+    redirect: 'follow',
+  };
+  useEffect(() => {
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=lofi&key=AIzaSyANJl-HNNmgsKJ-Yb0qUd3XS5uON39aheU',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+  }, []);
   return (
-    <ItemBox>
-      <Thumbnails>
-        <img src={'https://via.placeholder.com/120x90.jpg'} alt="sample" />
-      </Thumbnails>
-      <TextArea>
-        <strong>타이틀자리</strong>
-        <p>게시자</p>
-      </TextArea>
-    </ItemBox>
+    <a href="./">
+      <ItemBox>
+        <Thumbnails>
+          <img src={'https://via.placeholder.com/120x90.jpg'} alt="sample" />
+        </Thumbnails>
+        <TextArea>
+          <strong>타이틀자리</strong>
+          <p>게시자</p>
+        </TextArea>
+      </ItemBox>
+    </a>
   );
 };
 
