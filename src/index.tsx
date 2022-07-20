d69ccbf36768d077d0373e1d7f055ccf2805d163
ejjang <cuttlefish77777@gmail.com>
2022-07-20 13:25:33 +0900

    eslint, prettier setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import GlobalStyle from 'styles/globalStyles';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
