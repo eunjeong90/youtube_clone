b191bde51c0997c80f0dbc49aad032421dce2853
ejjang <cuttlefish77777@gmail.com>
2022-07-22 15:08:59 +0900

    youtube api fetch code type 추가

---
+++ src/service/youtube.ts
@@ -0,0 +1,32 @@
+export default class YoutubeApi {
+  getRequestOptions: { method: string; redirect: RequestRedirect };
+  key: string | undefined;
+  constructor(key: string | undefined) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  async mostPopular() {
+    const res = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await res.json();
+    return result.items;
+  }
+
+  async search(query: string) {
+    const res = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&q=${query}&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await res.json();
+    return result.items.map((item: { id: { videoId: string } }) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
+  }
+}
