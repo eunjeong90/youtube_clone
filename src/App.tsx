c23e4e4fa93ee4401f891c880be4cecd9ab406e6
ejjang <cuttlefish77777@gmail.com>
2022-08-04 17:20:03 +0900

    app 컴포넌트에서 axios 데이터 받아오기 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import './app.css';
 import GlobalStyle from 'src/globalStyles';
 import Container from 'src/components/container';
@@ -14,35 +15,73 @@ type DateType = {
   videoId: string;
 };
 
-function App({ youtube }: any) {
-  const [videoItem, setVideoItem] = useState<any>([]);
+function App() {
+  // const [videoItem, setVideoItem] = useState<any>([]);
+  // const [selectedVideo, setSelectedVideo] = useState<any | null>(null);
+  // const selectVideo = (videoItem: any) => {
+  //   setSelectedVideo(videoItem);
+  // };
+  // const search = useCallback(
+  //   (query: string) => {
+  //     youtube
+  //       .search(query) //
+  //       .then((items: any) => {
+  //         setVideoItem(items);
+  //         setSelectedVideo(null);
+  //       });
+  //   },
+  //   [youtube]
+  // );
+  // useEffect(() => {
+  //   youtube
+  //     .mostPopular() //
+  //     .then((items: any) => setVideoItem(items));
+  // }, [youtube]);
+  const youtube = process.env.REACT_APP_YOUTUBE_API_KEY;
+  const [videoItem, setVideoItem] = useState<any[]>([]);
+  const [searchItem, setSearchItem] = useState<any[]>([]);
   const [selectedVideo, setSelectedVideo] = useState<any | null>(null);
   const selectVideo = (videoItem: any) => {
     setSelectedVideo(videoItem);
   };
-  const search = (query: string) => {
-    youtube
-      .search(query) //
-      .then((items: any) => {
-        setVideoItem(items);
-        setSelectedVideo(null);
-      });
-  };
   useEffect(() => {
-    youtube
-      .mostPopular() //
-      .then((items: any) => setVideoItem(items));
+    const getMostPopularVideo = async () => {
+      try {
+        const response = await axios.get(
+          `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=statistics&type=video&chart=mostPopular&maxResults=25&key=${youtube}`
+        );
+        setVideoItem(response.data.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getMostPopularVideo();
   }, []);
+
+  // useEffect(() => {
+  //   const getSearchVideo = async () => {
+  //     const response = await axios.get(
+  //       // `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&q=${query}&key=${youtube}`
+  //       `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=25&key=${youtube}`,
+  //     );
+  //     setSearchItem(response.data);
+  //     searchItem.map((item) => ({
+  //       ...item,
+  //       id: item.id.videoId,
+  //     }));
+  //   };
+  //   getSearchVideo();
+  // }, []);
   return (
     <>
       <GlobalStyle />
-      <Header onSearch={search} />
+      {/* <Header onSearch={search} /> */}
       <Container>
-        {selectedVideo && (
+        {/* {selectedVideo && (
           <div>
             <ItemDetail videoItem={selectedVideo} />
           </div>
-        )}
+        )} */}
         <div>
           <ItemList videoItem={videoItem} onVideoClick={selectVideo} />
         </div>
